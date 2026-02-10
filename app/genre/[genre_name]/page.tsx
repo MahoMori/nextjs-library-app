9687462a71166115a6030b4fae28abf0724a42f0
MahoMori <tr27e1104@gmail.com>
2026-02-10 13:28:55 -0800

    Remove alert on server component

--- app/genre/[genre_name]/page.tsx
+++ app/genre/[genre_name]/page.tsx
@@ -3,21 +3,23 @@ import GenreBookList from "./GenreBookList";
 export default async function GenrePage({
   params,
 }: {
-  params: Promise<{ genre_name: string }>;
+  params: { genre_name: string };
 }) {
   let books = [];
   const fetchUrl =
     process.env.NODE_ENV === "production"
       ? "https://nextjs-library-app-p87v.vercel.app"
       : "http://localhost:3000";
-  const response = await fetch(
-    `${fetchUrl}/api/genre/${(await params).genre_name}`,
-    { method: "GET" }
-  );
+  const response = await fetch(`${fetchUrl}/api/genre/${params.genre_name}`, {
+    method: "GET",
+  });
   if (response.ok) {
     books = await response.json();
   } else {
-    alert("Failed to fetch books");
+    console.error("Failed to fetch books", {
+      status: response.status,
+      statusText: response.statusText,
+    });
   }
 
   return <GenreBookList initialBooks={books} />;
